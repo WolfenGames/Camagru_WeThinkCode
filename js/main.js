ef357d9695b0a143fedd48da5abb2f7045721f0e
Julian WOLF <jwolf@e4r15p8.wethinkcode.co.za>
2018-10-23 10:48:17 +0200

    auto commit

--- js/main.js
+++ js/main.js
@@ -1,26 +1,42 @@
-function CameraStuff()
-{
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-            try {
-                video.srcObject = stream;
-              } catch (error) {
-                video.src = window.URL.createObjectURL(stream);
-              }
-            video.play();
-        });
-    }
-}
-
 var video, context;
 
-window.onload = function(){
-    context = this.document.getElementById("canvas").getContext('2d');
-    video = document.querySelector('#video');
 
-    CameraStuff();
-    document.getElementById("snap").addEventListener("click", function(){
-        context.drawImage(video, 0, 0, video.width, video.height);
-    });
+function CameraStuff()
+{
+    video = document.querySelector('#video');
+    document.querySelector("#canvas").style.display = "none";
+    if (document.getElementById("Camera").style.display == "block")
+    {
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+                try {
+                    video.srcObject = stream;
+                } catch (error) {
+                    video.src = window.URL.createObjectURL(stream);
+                }
+                video.play();
+            });
+        }
+        document.getElementById("snap").addEventListener("click", click);
+    }
+}
+function click (){
+    document.querySelector("#canvas").style.display = "block";    
+    context = document.querySelector("#canvas").getContext('2d');
+    context.drawImage(video, 0, 0, video.width, video.height);
+    document.querySelector("#video").style.display = "none";
+}
+window.onload = function(){
+   
+    changeTab("Feed");
 }
 
+function changeTab(tabName) {
+    var i;
+    var x = document.getElementsByClassName("tab");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+    }
+    document.getElementById(tabName).style.display = "block";
+    CameraStuff();
+}
