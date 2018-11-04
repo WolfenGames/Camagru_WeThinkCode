4eacc5d2b4d273a055607f99326e352d20eba73b
Julian Wolf <julian.w16@gmail.com>
2018-11-04 23:55:28 +0200

    Update functions.php

--- profile/functions.php
+++ profile/functions.php
@@ -1,17 +1,17 @@
 <?php
 	set_include_path("../");
-    require_once("config/config.php");
+	require_once("config/config.php");
 
-    function login($email, $pass)
-    {
-        global $conn;
-        $pass = hash("sha512", $pass);
-        $query = "SELECT * FROM `camagru`.`users` WHERE `email` = :email AND `password` = :pass LIMIT 1;";
-        $stmt = $conn->prepare($query);
+	function login($email, $pass)
+	{
+		global $conn;
+		$pass = hash("sha512", $pass);
+		$query = "SELECT * FROM `camagru`.`users` WHERE `email` = :email AND `password` = :pass LIMIT 1;";
+		$stmt = $conn->prepare($query);
 		$stmt->bindParam(":pass", $pass);
 		$stmt->bindParam(":email", $email);
-        $stmt->execute();
-        $stmt->SetFetchMode(PDO::FETCH_ASSOC);
+		$stmt->execute();
+		$stmt->SetFetchMode(PDO::FETCH_ASSOC);
 		$user = $stmt->fetch();
 		if ($user)
 		{
@@ -26,53 +26,53 @@
 			}
 		}
 		return "No info";
-    }
+	}
 
-    function logout()
-    {
-        session_destroy();
-        header("Location: ./");
-    }
+	function logout()
+	{
+		session_destroy();
+		header("Location: ./");
+	}
 
-    function resend_verify($email)
-    {
-        global $conn;
-        try
-        {
-            $key = hash("sha512", $email . time());
-            $query = "UPDATE `camagru`.`users` SET `verifyKey` = :vkey, `isVerified` = 0 WHERE `email` = :email;";
-            $stmt = $conn->prepare($query);
-            $stmt->bindParam(":vkey", $key);
-            $stmt->bindParam(":email", $email);
-            $stmt->execute();
-            send_verify($email, $key);
-        }
-        catch (PDOException $e)
-        {
-            echo "Can not resend verification email -> " . $e->getMessage();
-        }
-    }
+	function resend_verify($email)
+	{
+		global $conn;
+		try
+		{
+			$key = hash("sha512", $email . time());
+			$query = "UPDATE `camagru`.`users` SET `verifyKey` = :vkey, `isVerified` = 0 WHERE `email` = :email;";
+			$stmt = $conn->prepare($query);
+			$stmt->bindParam(":vkey", $key);
+			$stmt->bindParam(":email", $email);
+			$stmt->execute();
+			send_verify($email, $key);
+		}
+		catch (PDOException $e)
+		{
+			echo "Can not resend verification email -> " . $e->getMessage();
+		}
+	}
 
-    function send_verify($email, $key)
-    {
-        $from = "admin@camagru.com";
-        $to = $email;
-        $subject = "Verify Camagru Account";
-        $message = "<html><body>";
+	function send_verify($email, $key)
+	{
+		$from = "admin@camagru.com";
+		$to = $email;
+		$subject = "Verify Camagru Account";
+		$message = "<html><body>";
 		$message .= "Please click on the following link to allow us to activate you account\n";
 		$loc = str_replace("?method=resend", "", $_SERVER['REQUEST_URI']);
 		$loc = str_replace("?method=login", "", $loc);
 		$loc = str_replace("?method=register", "", $loc);
-        $message .= "<a href='http://".$_SERVER['HTTP_HOST'] . $loc ."/../verify.php?verify=".$key."'><p>Click me!!</p></a>";
-        $message .= "</body></html>";
-        $headers = "From:" . $from . "\r\n";
-        $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-        $headers .= "X-Mailer: PHP/" . PHP_VERSION;
-        mail($to,$subject,$message, $headers);
-    }
+		$message .= "<a href='http://".$_SERVER['HTTP_HOST'] . $loc ."/../verify.php?verify=".$key."'><p>Click me!!</p></a>";
+		$message .= "</body></html>";
+		$headers = "From:" . $from . "\r\n";
+		$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+		$headers .= "X-Mailer: PHP/" . PHP_VERSION;
+		mail($to,$subject,$message, $headers);
+	}
 
-    function delete_account($email, $pass)
-    {
+	function delete_account($email, $pass)
+	{
 		global $conn;
 		try
 		{
@@ -86,23 +86,23 @@
 		{
 			echo "Can`t delete -> " . $e->getMessage();
 		}
-    }
+	}
 
-    function register($login, $email, $pass, $veripass)
-    {
-        global $conn;
-        if ($pass == $veripass)
-        {
-            try
-            {
-                $pass = hash("sha512", $pass);
-                $query = "INSERT INTO `camagru`.`users` (`username`, `email`, `password`, `verifyKey`) VALUES (:uname, :email, :pass, :vkey);";
-                $key = hash("sha512", $email . time());
-                $stmt = $conn->prepare($query);
-                $stmt->bindParam(":uname", $login);
-                $stmt->bindParam(":email", $email);
-                $stmt->bindParam(":pass", $pass);
-                $stmt->bindParam(":vkey", $key);
+	function register($login, $email, $pass, $veripass)
+	{
+		global $conn;
+		if ($pass == $veripass)
+		{
+			try
+			{
+				$pass = hash("sha512", $pass);
+				$query = "INSERT INTO `camagru`.`users` (`username`, `email`, `password`, `verifyKey`) VALUES (:uname, :email, :pass, :vkey);";
+				$key = hash("sha512", $email . time());
+				$stmt = $conn->prepare($query);
+				$stmt->bindParam(":uname", $login);
+				$stmt->bindParam(":email", $email);
+				$stmt->bindParam(":pass", $pass);
+				$stmt->bindParam(":vkey", $key);
 				$stmt->execute();
 				if ($stmt)
 				{
@@ -112,14 +112,14 @@
 				else
 					echo "InvalidRegistration";
 				
-            }
-            catch (PDOException $e)
-            {
-                echo "Failed to register -> " . $e->getMessage();
-            }
-        }
-        else
-            echo "Password is invalid!\n";
+			}
+			catch (PDOException $e)
+			{
+				echo "Failed to register -> " . $e->getMessage();
+			}
+		}
+		else
+			echo "Password is invalid!\n";
 	}
 
 	function like($id)
